aff4445d0bdc98c558fd9bd0c9011219aa263c03
Muhammad Azahrul Ramadhan <azahrulsmavo@gmail.com>
2026-01-05 22:29:45 +0700

    style(web): add link to details in Personal page

--- src/pages/Personal.jsx
+++ src/pages/Personal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { useLanguage } from '../context/LanguageContext';
